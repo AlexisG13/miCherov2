6ef7d45ec9665043ef19dbfbe00032146e73f842
Alexis Gomez <gomezalexisj13@gmail.com>
2019-12-23 12:27:36 -0600

    :sparkles: Add ormconfig.json file and import TypeORMModule
    Add ormconfig.json file and import TypeORMModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { NewsModule } from './news/news.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [NewsModule, ConfigModule.forRoot({ isGlobal: true }), AuthModule, UsersModule],
+  imports: [
+    NewsModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UsersModule,
+    TypeOrmModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
